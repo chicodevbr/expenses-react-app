7009e08cbf8d4bd451ccb7f7d16f7e72458966c0
chicodevbr <chicodevbr@gmail.com>
2021-10-16 22:06:23 -0300

    outputting conditional render

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expense from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.12,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'iPhone',
+    amount: 1799,
+    date: new Date(2021, 7, 14),
+  },
+  {
+    id: 'e5',
+    title: 'MacBook Pro',
+    amount: 2799,
+    date: new Date(2021, 2, 23),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.12,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'iPhone',
-      amount: 1799,
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: 'e5',
-      title: 'MacBook Pro',
-      amount: 2799,
-      date: new Date(2021, 2, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
